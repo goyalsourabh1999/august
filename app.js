a4c171c2d541d4592f268289c3ef1a6f956bef89
Jasbir <mr.jasbirsingh96@gmail.com>
2020-02-09 19:46:17 +0530

    webhooks implemented

--- app.js
+++ app.js
@@ -7,14 +7,17 @@ const cookieParser = require("cookie-parser");
 const planRouter = require("./routers/planRouter");
 const userRouter = require("./routers/userRouter");
 const viewRouter = require("./routers/viewRouter");
-const bookingRouter=require("./routers/bookingRouter");
+const bookingRouter = require("./routers/bookingRouter");
+const bookingController = require("./controllers/bookingController");
 // converts buffer to json
-// 
-app.use(express.urlencoded({extended:true}));
+// app.use(bodyParser.raw({ type: 'application/json' }));
+app.post("/webhook-checkout", bookingController.createBooking);
+
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // => static files
 app.use(express.static("public"));
-app.use("/plans",express.static("public"))
+app.use("/plans", express.static("public"))
 
 app.use(cookieParser());
 
@@ -23,17 +26,17 @@ app.set("view engine", "pug");
 app.set("views", "views");
 
 
-app.use(function(req,res,next){
+app.use(function (req, res, next) {
   console.log("cookies");
   console.log(req.cookies)
   console.log("end");
-next();
+  next();
 })
 app.use("/", viewRouter);
 
 app.use("/api/plans", planRouter);
 app.use("/api/users", userRouter);
-app.use("/api/bookings",bookingRouter);
+app.use("/api/bookings", bookingRouter);
 // app.get("/plans",);
 // createPlans
 // plans/1
@@ -44,7 +47,7 @@ app.use("/api/bookings",bookingRouter);
 
 // user
 
-const port=process.env.PORT||3000
+const port = process.env.PORT || 3000
 app.listen(port, () => {
   console.log("Server is listening at port 3000");
 });
