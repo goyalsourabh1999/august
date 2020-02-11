89d137463b1d3b9362d65f6f8d8875b10bc4233a
Jasbir <mr.jasbirsingh96@gmail.com>
2020-02-11 13:21:05 +0530

    Revert "added sharp"
    This reverts commit 4b7dd2433e3907de5f782e6687e47b427916a153.

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,4 @@
 const userModel = require("../models/userModel");
-const sharp =require("sharp");
 module.exports.getUser = async function(req, res) {
   const { id } = req.body;
   const user = await userModel.findById(id);
@@ -8,17 +7,15 @@ module.exports.getUser = async function(req, res) {
 // update
 module.exports.updateUser = async function(req, res) {
   const id = req.params.id;
-  req.file.filename = `user-${req.user.id}-${Date.now()}.jpeg`;
-  await sharp(req.file.buffer)
-    .resize(500, 500)
-    .toFormat('jpeg')
-    .jpeg({ quality: 90 })
-    .toFile(`public/img/users/${req.file.filename}`);
+  console.log(id);
+  console.log(req.body);
   const photo=req.file.filename;
   req.body.photo=photo;
   const user = await userModel.findOneAndUpdate({ _id: id }, req.body, {
     new: true
   });
   console.log(user);
+// express => redirect
   res.redirect("/me");
+  // const id = user.id;
 };
